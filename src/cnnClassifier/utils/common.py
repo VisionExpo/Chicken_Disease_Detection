9f0efda5d5572588d20b4195a34eaddf258ac00c
Vision Expo <gorulevishal984@gmail.com>
2024-10-26 08:02:40 +0530

    prepare callbacks completed

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -9,15 +9,35 @@ from box import ConfigBox
 from pathlib import Path
 from typing import Any
 import base64
+import logging
 
 
+
+logger = logging.getLogger(__name__)
 
 @ensure_annotations
-def read_yaml(file_path: Path) -> ConfigBox:
-    with open(file_path, "r") as file:
-        config = yaml.safe_load(file)
-        logger.debug(f"Loaded YAML content: {config}")
-        return config
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
     
 
 
